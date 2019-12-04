2f781653d2404bf6347e0a26658b33f44acc9b71
zhaiyu <zhaiyu@qianxin.com>
2019-12-04 11:07:39 +0800

    :bug: fix(master): 修复deepin系应用左右分屏有空挡的问题，不过还是存在小问题待完善
    Signed-off-by: zhaiyu <zhaiyu@qianxin.com>

--- win_size.sh
+++ win_size.sh
@@ -7,16 +7,30 @@
 #
 #         Author: zishu, zishu@qq.com
 #        Created: 2019-10-12 16:40:18
-#  Last Modified: 2019-11-22 09:49:59
+#  Last Modified: 2019-12-04 11:02:59
 #
 # =================================================================
 
 com=$1
+
+# 获取当前激活窗口名称
+active_win_name=`xprop -id $(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2) _NET_WM_NAME`
+
+# 将最大化窗口还原，否则无法调整大小
 wmctrl -r :ACTIVE: -b remove,maximized_vert,maximized_horz
+
 if [[ $com == "left" ]]; then
-    wmctrl -r :ACTIVE: -e 0,0,0,960,1080
+    if [[ ${active_win_name} =~ "深度" ]]; then
+        wmctrl -r :ACTIVE: -e 0,-50,-50,1060,1190
+    else
+        wmctrl -r :ACTIVE: -e 0,0,0,960,1080
+    fi
 elif [[ $com == "right" ]]; then
-    wmctrl -r :ACTIVE: -e 0,960,0,960,1080
+    if [[ ${active_win_name} =~ "深度" ]]; then
+        wmctrl -r :ACTIVE: -e 0,910,-50,1060,1190
+    else
+        wmctrl -r :ACTIVE: -e 0,960,0,960,1080
+    fi
 fi
 
 #define the height in px of the top system-bar:
