e5cddf394791b5b42d82d3581c6d2759623e0cc5
zhaiyu <zhaiyu@qianxin.com>
2020-01-13 14:28:50 +0800

    :bug: fix(master): 兼容多分辨率的环境
    Signed-off-by: zhaiyu <zhaiyu@qianxin.com>

--- win_size.sh
+++ win_size.sh
@@ -7,7 +7,7 @@
 #
 #         Author: zishu, zishu@qq.com
 #        Created: 2019-10-12 16:40:18
-#  Last Modified: 2019-12-04 13:49:44
+#  Last Modified: 2020-01-11 21:14:23
 #
 # =================================================================
 
@@ -16,25 +16,45 @@ com=$1
 # 获取当前激活窗口名称
 active_win_name=`xprop -id $(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2) _NET_WM_NAME`
 
+SCREEN_WIDTH=$(xwininfo -root | awk '$1=="Width:" {print $2}')
+SCREEN_HEIGHT=$(xwininfo -root | awk '$1=="Height:" {print $2}')
+
 # 将最大化窗口还原，否则无法调整大小
 wmctrl -r :ACTIVE: -b remove,maximized_vert,maximized_horz
 
+
+n_width=$(($SCREEN_WIDTH/2))
+n_height=$(($SCREEN_HEIGHT))
 if [[ $com == "left" ]]; then
+    n_x=0
+    n_y=0
     if [[ ${active_win_name} =~ "深度终端" ]]; then
-        wmctrl -r :ACTIVE: -e 0,-50,-50,1060,1190
+        n_x=-50
+        n_y=-50
+        n_width=$(($n_width+100))
+        n_height=$(($n_height+110))
     elif [[ ${active_win_name} =~ "深度文件管理器" ]]; then
-        wmctrl -r :ACTIVE: -e 0,-60,-44,1082,1200
-    else
-        wmctrl -r :ACTIVE: -e 0,0,0,960,1080
+        n_x=-59
+        n_y=-44
+        n_width=$(($n_width+121))
+        n_height=$(($n_height+120))
     fi
+    wmctrl -r :ACTIVE: -e 0,$n_x,$n_y,$n_width,$n_height
 elif [[ $com == "right" ]]; then
+    n_x=$n_width
+    n_y=0
     if [[ ${active_win_name} =~ "深度终端" ]]; then
-        wmctrl -r :ACTIVE: -e 0,910,-50,1060,1190
+        n_x=$(($n_x-50))
+        n_y=-50
+        n_width=$(($n_width+100))
+        n_height=$(($n_height+110))
     elif [[ ${active_win_name} =~ "深度文件管理器" ]]; then
-        wmctrl -r :ACTIVE: -e 0,901,-44,1082,1200
-    else
-        wmctrl -r :ACTIVE: -e 0,960,0,960,1080
+        n_x=$(($n_x-59))
+        n_y=-44
+        n_width=$(($n_width+121))
+        n_height=$(($n_height+120))
     fi
+    wmctrl -r :ACTIVE: -e 0,$n_x,$n_y,$n_width,$n_height
 fi
 
 #define the height in px of the top system-bar:
